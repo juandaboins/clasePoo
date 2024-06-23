ba01519d97cc2f82b1c280bba7390735b5ff4f5f
juan daboin <juandaboins@gmail.com>
2024-06-23 16:47:33 -0300

    bucles ejercios do while

--- src/clases/doWhile.java
+++ src/clases/doWhile.java
@@ -1,4 +1,51 @@
 package clases;
 
 public class doWhile {
+//    ejercicio nº3
+//    int suma = 0;
+//    int contador = 0;
+//    int numero;
+//
+//    // Utilizando un bucle do-while para la entrada de datos
+//        do {
+//        System.out.print("Introduce un número positivo (o un número negativo para terminar): ");
+//        numero = scanner.nextInt();
+//
+//        if (numero >= 0) {
+//            suma += numero;
+//            contador++;
+//        }
+//    } while (numero >= 0);
+//
+//        if (contador > 0) {
+//        double media = (double) suma / contador;
+//        System.out.println("La media de los números introducidos es: " + media);
+//    } else {
+//        System.out.println("No se introdujeron números positivos.");
+//    }
+
+
+//    ejercicio nº 4
+//    int intentos = 0;
+//    boolean abierta = false;
+//
+//        do {
+//        System.out.print("Introduce la combinación de 4 cifras: ");
+//        int combinacion = scanner.nextInt();
+//        intentos++;
+//
+//        if (combinacion == COMBINACION_CORRECTA) {
+//            System.out.println("La caja fuerte se ha abierto satisfactoriamente.");
+//            abierta = true;
+//        } else {
+//            System.out.println("Lo siento, esa no es la combinación.");
+//            if (intentos < MAX_INTENTOS) {
+//                System.out.println("Te quedan " + (MAX_INTENTOS - intentos) + " intentos.");
+//            }
+//        }
+//    } while (intentos < MAX_INTENTOS && !abierta);
+//
+//        if (!abierta) {
+//        System.out.println("Has agotado todos los intentos. La caja fuerte permanece cerrada.");
+//    }
 }
