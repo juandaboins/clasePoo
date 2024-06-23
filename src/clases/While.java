034f34219d70a7a83512eb0110ee30d8119386a7
juan daboin <juandaboins@gmail.com>
2024-06-23 16:17:03 -0300

    bucles ejercios while

---
+++ src/clases/While.java
@@ -0,0 +1,47 @@
+package clases;
+
+public class While {
+
+    //ejercio de while nº1
+//    int intentos = 4;
+//    int combinacion;
+//
+//        while (intentos > 0) {
+//        System.out.print("Introduce la combinación de 4 cifras: ");
+//        combinacion = scanner.nextInt();
+//
+//        if (combinacion == COMBINACION_CORRECTA) {
+//            System.out.println("La caja fuerte se ha abierto satisfactoriamente.");
+//            break;
+//        } else {
+//            intentos--;
+//            if (intentos > 0) {
+//                System.out.println("Lo siento, esa no es la combinación. Te quedan " + intentos + " intentos.");
+//            } else {
+//                System.out.println("Lo siento, esa no es la combinación. Has agotado todos los intentos.");
+//            }
+//        }
+//    }
+
+//    ejercio de while nº 2
+//    int suma = 0;
+//    int contador = 0;
+//
+//        System.out.print("Introduce un número positivo (o un número negativo para terminar): ");
+//    int numero = scanner.nextInt();
+//
+//        while (numero >= 0) {
+//        suma += numero;
+//        contador++;
+//
+//        System.out.print("Introduce otro número positivo (o un número negativo para terminar): ");
+//        numero = scanner.nextInt();
+//    }
+//
+//        if (contador > 0) {
+//        double media = (double) suma / contador;
+//        System.out.println("La media de los números introducidos es: " + media);
+//    } else {
+//        System.out.println("No se introdujeron números positivos.");
+//    }
+}
