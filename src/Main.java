e288d23e3bfb2d348488d314bf0151f8c18543a6
juan daboin <juandaboins@gmail.com>
2024-06-23 15:40:15 -0300

    bucles ejercios for

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,66 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+//        //ejercio nº 1
+//      for (int i = 0; i <= 100; i += 5) {
+//          System.out.println(i);
+//      }
+
+//        //ejercio nº 2
+//        for (int i = 320; i >= 160; i -= 20) {
+//            System.out.println(i);
+//        }
+
+        //ejercio nº 3
+//        int combinacionCorrecta = 1234;
+//        int intentos = 4;
+//
+//        for (int i = 1; i <= intentos; i++) {
+//            System.out.print("Introduce la combinación de 4 cifras: ");
+//            int combinacion = scanner.nextInt();
+//
+//            if (combinacion == combinacionCorrecta) {
+//                System.out.println("La caja fuerte se ha abierto satisfactoriamente.");
+//                break;
+//            } else {
+//                if (i < intentos) {
+//                    System.out.println("Lo siento, esa no es la combinación. Te quedan " + (intentos - i) + " intentos.");
+//                } else {
+//                    System.out.println("Lo siento, esa no es la combinación. Has agotado todos los intentos.");
+//                }
+//            }
+//        }
+
+        //ejercio nº 4
+
+//        int suma = 0;
+//        int contador = 0;
+//
+//        // Utilizando un bucle for para la entrada de datos
+//        for (;;) { // Bucle infinito que se rompe con una condición interna
+//            System.out.print("Introduce un número positivo (o un número negativo para terminar): ");
+//            int numero = scanner.nextInt();
+//
+//            if (numero < 0) {
+//                        break; // Salir del bucle si se introduce un número negativo
+//            }
+//
+//            suma += numero;
+//            contador++;
+//        }
+//
+//            if (contador > 0) {
+//                double media = (double) suma / contador;
+//                System.out.println("La media de los números introducidos es: " + media);
+//            } else {
+//                System.out.println("No se introdujeron números positivos.");
+//            }
+//                scanner.close();
+//            }
+        }
     }
-}
